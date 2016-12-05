cfff9688db54109ce27d31d0a365f6591017adb3
yukinami <yuki-nami@hotmail.com>
2016-12-05 14:06:08 +0800

    migration使用liquibase

--- src/main/resources/db/migration/develop/V2__users.sql
+++
@@ -1,5 +0,0 @@
-create table authorities (
-  username varchar_ignorecase(50) not null,
-  authority varchar_ignorecase(50) not null
-);
-create unique index ix_auth_username on authorities (username,authority);
